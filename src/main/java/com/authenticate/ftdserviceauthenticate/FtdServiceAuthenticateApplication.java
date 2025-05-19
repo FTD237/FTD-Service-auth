2ba6dba70155db080368f40b75dfd68e88fca7d5
logan496 <fils.tsajio@2027.ucac-icam.com>
2025-05-19 13:05:40 +0100

    Project init

---
+++ src/main/java/com/authenticate/ftdserviceauthenticate/FtdServiceAuthenticateApplication.java
@@ -0,0 +1,13 @@
+package com.authenticate.ftdserviceauthenticate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtdServiceAuthenticateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtdServiceAuthenticateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/authenticate/ftdserviceauthenticate/FtdServiceAuthenticateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authenticate.ftdserviceauthenticate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtdServiceAuthenticateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
